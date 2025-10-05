d9a11c7ab78e1731df400f6aeed496a2245d7a97
samu019 <slimmysamuelmbaesonomangue@gmail.com>
2025-10-06 02:58:10 +0300

    fix: Remove flask configuration from django __init__.py

--- restaurant_platform/__init__.py
+++ restaurant_platform/__init__.py
@@ -1,16 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# Crea el objeto db para la base de datos
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-
-    # Configuración de la base de datos (asegúrate de tener configurada la URI)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'  # Si usas SQLite
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactiva las modificaciones de seguimiento
-
-    db.init_app(app)
-
-    return app
+﻿
