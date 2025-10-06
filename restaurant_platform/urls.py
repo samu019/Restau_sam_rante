706b914f2435ec5e78aa7aeb98b1c00eac41bbea
samu019 <slimmysamuelmbaesonomangue@gmail.com>
2025-10-06 11:18:58 +0300

    fix: Correct import statements in urls.py - include from django.urls

--- restaurant_platform/urls.py
+++ restaurant_platform/urls.py
@@ -1,7 +1,8 @@
 # restaurant_platform/urls.py
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import RedirectView, include
+from django.urls import path, include
+from django.views.generic import RedirectView
 from restaurant import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -103,3 +104,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
